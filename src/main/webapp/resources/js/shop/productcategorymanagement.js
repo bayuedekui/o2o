1175b189f0ef24cfba76b1acb0415194eb3e4efa
zhengkui <468360786@qq.com>
2018-09-06 00:32:18 +0800

    前端添加功能做好,样式还有部分问题,功能还没有完全调通

--- src/main/webapp/resources/js/shop/productcategorymanagement.js
+++ src/main/webapp/resources/js/shop/productcategorymanagement.js
@@ -18,21 +18,57 @@ $(function () {
 
     }
 
-
     /**
      * 展现后台传过来的商品列表数据
      */
     function handleProductCategoryList(data) {
         var html = "";
-        data.map(function(item,index){
-            html+='<div class="row row-shop">' +
-                '<div class="col-40">'+item.productCategoryName+'</div>' +
-                '<div class="col-40">'+item.priority+'</div>' +
-                '<div col-20><button id="deleteProductCategory('+item.productCategoryId+')" class="button button-small button-info" >删除</></div>' +
-                '</div>';
+        data.map(function (item, index) {
+            html += '<div class="row row-shop">'
+                + '<div class="col-40">' + item.productCategoryName + '</div>'
+                + '<div class="col-40">' + item.priority + '</div>'
+                + '<div col-20><a id="deleteProductCategory(' + item.productCategoryId + ')" class="button button-small button-info" >删除</a></div>'
+                + '</div>';
         });
 
-        $(".productCategory-wrap").html(html);
+        $(".productCategory-wrap").append(html);
     }
+
+    //点击新增按钮事件
+    $("#new").click(function () {
+        var tempHtml = '<div class="row row-product-category temp">'
+            + '<div class="col-40"><input class="category-input category" type="text" placeholder="商品分类名"></div>'
+            + '<div class="col-40"><input class="category-input priority" type="number" placeholder="优先级"></div>'
+            + '<div class="col-20"><a href="#" class="button button-small delete">删除</a></div>'
+            + '</div>';
+        $(".productCategory-wrap").append(tempHtml);
+    });
+    //点击提交按钮事件
+    $("#submit").click(function () {
+        var tempArr = $(".temp");
+        var productCategoryList = [];
+        tempArr.map(function (index, item) {
+            var tempProCateObj = {};
+            tempProCateObj.productCategoryName = $(item).find(".category").val();
+            tempProCateObj.priority = $(item).find(".priority").val();
+            productCategoryList.push(tempProCateObj);
+        });
+        $.ajax({
+            url: "o2o/shopadmn/addproductcategorys",
+            type: "POST",
+            data: JSON.stringify(productCategoryList),
+            success: function (data) {
+                if (data.success) {
+                    $.toast("提交成功!");
+                    //重新展现增加后的商品列表
+                    getShopCategory();
+                } else {
+                    $.toast("提交失败!");
+                }
+            }
+
+        });
+
+    });
 
 });
